7a483bfa325028f7a5ba7bd9c4502e6857904109
Nicola <lanzoni.nicola@gmail.com>
2021-02-28 17:49:16 -0300

    layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,44 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
+      home: Challenge(),
     ),
   );
+}
+
+class Challenge extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Row(
+        children: [
+          Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.red,
+              )),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.teal,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(width: 100, height: 100, color: Colors.yellow),
+                  Container(width: 100, height: 100, color: Colors.green[600]),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
 }
